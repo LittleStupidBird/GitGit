af605249d8d2c1725d452a16159640ed2c128623
LittleStupidBird <125426033@qq.com>
2019-03-16 09:49:11 +0800

    建立新工程

---
+++ LittleBird/LittleBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleBird
+//
+//  Created by Allen on 2019/3/16.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
